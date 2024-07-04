87e9863f3442fae4067a06e941e3677f40e4a68e
JotapeSo <joaopaulosouza.57@gmail.com>
2024-07-04 10:52:19 -0300

    Add files via upload

---
+++ teste.form/cadastro.php
@@ -0,0 +1,13 @@
+<?php
+
+include("./conexao.php");
+
+$nome_comp = $_POST["nome_comp"];
+$idade = $_POST["idade"];
+$profissao = $_POST["profissao"];
+
+echo $nome_comp;
+echo $idade;
+echo $profissao;
+
+?>
